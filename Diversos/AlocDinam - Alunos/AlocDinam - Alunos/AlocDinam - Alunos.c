2f516c9b054b24e2fca3cf26538fecc7170d488c
Brayan <brayanws.2001@gmail.com>
2025-08-24 12:06:30 -0300

    feat: cadastro dinamico e apresentação dos dados adicionados

--- Diversos/AlocDinam - Alunos/AlocDinam - Alunos/AlocDinam - Alunos.c
+++ Diversos/AlocDinam - Alunos/AlocDinam - Alunos/AlocDinam - Alunos.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 typedef struct Alunos
 {
@@ -9,23 +10,63 @@ typedef struct Alunos
 
 } Alunos;
 
-Alunos **ptrAlunos();
+Alunos* ptrAlunos(int* qtdAlunos);
+void cadastraAlunos(Alunos* alunos, int qtdAlunos);
+void mostraAlunos(Alunos* alunos, int qtdAlunos);
 
-//função para cadastrar alunos
-
-// função para mostrar alunos
 
 int main()
 {
-    
-}
+    setlocale(LC_ALL, "portuguese");
 
-Alunos ** ptrAlunos()
-{
     int qtdAlunos;
 
-    printf("Quantos alunos serão cadastrados?");
-    scanf_s("%d", &qtdAlunos);
+    Alunos* alunos = ptrAlunos(&qtdAlunos);
 
-    return (Alunos*)calloc(qtdAlunos, sizeof(Alunos));
+    cadastraAlunos(alunos, qtdAlunos);
+    mostraAlunos(alunos, qtdAlunos);
+    
+    free(alunos);
+
+    return 0;
+}
+
+Alunos* ptrAlunos(int* qtdAlunos)
+{
+    printf("Quantos alunos serao cadastrados? ");
+    scanf_s("%d", qtdAlunos);
+    while (getchar() != '\n');
+
+    return (Alunos*)calloc(*qtdAlunos, sizeof(Alunos));
+}
+
+void cadastraAlunos(Alunos* alunos, int qtdAlunos)
+{
+    for (int i = 0; i < qtdAlunos; i++)
+    {
+        printf("\nAluno %d", i+1);
+
+        printf("\nInsira a matricula: ");
+        scanf_s("%u", &alunos[i].matricula);
+        while (getchar() != '\n');
+
+        printf("Insira o nome: ");
+        fgets(alunos[i].nome, 20, stdin);
+        
+        printf("Insira o ano de nascimento: ");
+        scanf_s("%u", &alunos[i].anoNascimento);
+        while (getchar() != '\n');
+    }
+}
+
+void mostraAlunos(Alunos* alunos, int qtdAlunos)
+{
+    for (int i = 0; i < qtdAlunos; i++)
+    {
+        printf("\nAluno %d", i + 1);
+        printf("\n%u", alunos[i].matricula);
+        printf("\n%s", alunos[i].nome);
+        printf("%u", alunos[i].anoNascimento);
+        printf("\n");
+    }
 }
